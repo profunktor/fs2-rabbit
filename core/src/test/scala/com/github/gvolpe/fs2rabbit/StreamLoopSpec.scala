1fb851014d07713d3571a21f47553514844ea137
Gabi Volpe <volpegabriel@gmail.com>
2017-06-16 17:22:00 +0200

    Anonymous class of EffectScheduler inside test that needs it instead of object defined as a single file.

--- core/src/test/scala/com/github/gvolpe/fs2rabbit/StreamLoopSpec.scala
+++ core/src/test/scala/com/github/gvolpe/fs2rabbit/StreamLoopSpec.scala
@@ -1,9 +1,10 @@
 package com.github.gvolpe.fs2rabbit
 
-import cats.effect.IO
+import cats.effect.{Effect, IO}
 import fs2._
 import org.scalatest.{FlatSpecLike, Matchers}
 
+import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 import scala.util.Random
 
@@ -11,7 +12,14 @@ class StreamLoopSpec extends FlatSpecLike with Matchers {
 
   implicit val ec = scala.concurrent.ExecutionContext.Implicits.global
   implicit val s  = fs2.Scheduler.fromFixedDaemonPool(2, "restarter")
-  implicit val es = IOEffectScheduler
+
+  implicit val es = new EffectScheduler[IO] {
+    override def schedule[A](body: IO[A], delay: FiniteDuration)
+                            (implicit ec: ExecutionContext, s: Scheduler, F: Effect[IO]) = {
+      IO.async[Unit] { cb => s.scheduleOnce(delay)(cb(Right(()))) }.flatMap(_ => body)
+    }
+    override def unsafeRunSync(effect: IO[Unit]) = effect.unsafeRunSync()
+  }
 
   it should "run a stream until it's finished" in {
     val sink = Fs2Utils.liftSink[IO, Int](n => IO(println(n)))
