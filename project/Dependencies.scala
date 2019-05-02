742e384fa8b39508040821be29f608f025a1e6d4
Scala steward <me@scala-steward.org>
2019-05-02 04:01:17 +0200

    Update cats-effect to 1.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val catsEffect = "1.2.0"
+    val catsEffect = "1.3.0"
     val fs2        = "1.0.4"
     val circe      = "0.11.1"
     val amqpClient = "5.7.0"
