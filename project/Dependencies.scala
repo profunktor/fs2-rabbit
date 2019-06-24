a4bfc17c00ce68b725299a446400abeae69a6d5d
Changlin Li <mail@changlinli.com>
2019-06-24 19:39:08 -0400

    Refactor way that dependencies are expressed
    The old way I had didn't scale up and was quite messy for trying to deal
    with a lot of different dependency versions. This makes it a bit
    cleaner.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,6 +2,36 @@ import sbt._
 
 object Dependencies {
 
+  sealed trait Version {
+    val catsEffect = "1.3.1"
+    val fs2        = "1.0.5"
+    val circe      = "0.11.1"
+    val amqpClient = "5.7.1"
+    val logback    = "1.2.3"
+
+    val kindProjector    = "0.10.3"
+    val betterMonadicFor = "0.3.0"
+
+    val scalaTest  = "3.0.8"
+    val scalaCheck = "1.14.0"
+  }
+
+  object Scala211Versions extends Version {
+    val monix      = "3.0.0-RC3"
+    val zio        = "1.0.0-RC8-4"
+  }
+
+  object Scala212Versions extends Version {
+    val monix      = "3.0.0-RC3"
+    val zio        = "1.0.0-RC8-4"
+  }
+
+  object Scala213Versions extends Version {
+    override val catsEffect = "2.0.0-M4"
+    override val fs2        = "1.1.0-M1"
+    override val circe      = "0.12.0-M3"
+  }
+
   object Versions {
     val catsEffect = "2.0.0-M4"
     val fs2        = "1.1.0-M1"
@@ -18,50 +48,19 @@ object Dependencies {
     val scalaCheck = "1.14.0"
   }
 
-  object Libraries {
-    sealed trait CurrentScalaVersion
-    case object Scala213 extends CurrentScalaVersion
-    case object Scala212Or211 extends CurrentScalaVersion
-    case object UnknownScalaVersion extends CurrentScalaVersion
+  sealed abstract class Library[V <: Version](val version: V) {
+    def circe(artifact: String): ModuleID = "io.circe" %% artifact % version.circe
 
-    def compilingForScala213(scalaVersionStr: String): CurrentScalaVersion =
-      CrossVersion.partialVersion(scalaVersionStr) match {
-        case Some((2, 13)) => Scala213
-        case Some((2, 12)) => Scala212Or211
-        case Some((2, 11)) => Scala212Or211
-        case _ => UnknownScalaVersion
-      }
-
-    def circe(artifact: String): ModuleID = "io.circe" %% artifact % Versions.circe
-    def zio(artifact: String): ModuleID = "org.scalaz" %% artifact % Versions.zio
-
-    lazy val amqpClient = "com.rabbitmq"  % "amqp-client"  % Versions.amqpClient
-    lazy val catsEffect = "org.typelevel" %% "cats-effect" % Versions.catsEffect
-    lazy val fs2Core    = "co.fs2"        %% "fs2-core"    % Versions.fs2
+    lazy val amqpClient = "com.rabbitmq"  % "amqp-client"  % version.amqpClient
+    lazy val catsEffect = "org.typelevel" %% "cats-effect" % version.catsEffect
+    lazy val fs2Core    = "co.fs2"        %% "fs2-core"    % version.fs2
 
     // Compiler
-    lazy val kindProjector    = "org.typelevel" % "kind-projector"      % Versions.kindProjector cross CrossVersion.binary
-    lazy val betterMonadicFor = "com.olegpy"    %% "better-monadic-for" % Versions.betterMonadicFor
+    lazy val kindProjector    = "org.typelevel" % "kind-projector"      % version.kindProjector cross CrossVersion.binary
+    lazy val betterMonadicFor = "com.olegpy"    %% "better-monadic-for" % version.betterMonadicFor
 
     // Examples
-    def monix(scalaVersionStr: String): Option[ModuleID] = compilingForScala213(scalaVersionStr) match {
-      case Scala212Or211 => Some("io.monix" %% "monix" % Versions.monix)
-      case Scala213 => None
-      case UnknownScalaVersion => None
-    }
     lazy val logback = "ch.qos.logback" % "logback-classic" % Versions.logback
-
-    def zioCore(scalaVersionStr: String): Option[ModuleID] = compilingForScala213(scalaVersionStr) match {
-      case Scala212Or211 => Some(zio("scalaz-zio"))
-      case Scala213 => None
-      case UnknownScalaVersion => None
-    }
-
-    def zioCats(scalaVersionStr: String): Option[ModuleID] = compilingForScala213(scalaVersionStr) match {
-      case Scala212Or211 => Some(zio("scalaz-zio-interop-cats"))
-      case Scala213 => None
-      case UnknownScalaVersion => None
-    }
 
     // Json libraries
     lazy val circeCore    = circe("circe-core")
@@ -69,8 +68,28 @@ object Dependencies {
     lazy val circeParser  = circe("circe-parser")
 
     // Scala test libraries
-    lazy val scalaTest  = "org.scalatest"  %% "scalatest"  % Versions.scalaTest
-    lazy val scalaCheck = "org.scalacheck" %% "scalacheck" % Versions.scalaCheck
+    lazy val scalaTest  = "org.scalatest"  %% "scalatest"  % version.scalaTest
+    lazy val scalaCheck = "org.scalacheck" %% "scalacheck" % version.scalaCheck
   }
+
+  case object Scala211Libraries extends Library(Scala211Versions) {
+    def zio(artifact: String): ModuleID = "dev.zio" %% artifact % version.zio
+
+    // Example Libraries
+    lazy val monix = "io.monix" %% "monix" % version.monix
+    lazy val zioCore = zio("zio")
+    lazy val zioCats = zio("zio-interop-cats")
+  }
+
+  case object Scala212Libraries extends Library(Scala212Versions) {
+    def zio(artifact: String): ModuleID = "dev.zio" %% artifact % version.zio
+
+    // Example Libraries
+    lazy val monix = "io.monix" %% "monix" % version.monix
+    lazy val zioCore = zio("zio")
+    lazy val zioCats = zio("zio-interop-cats")
+  }
+
+  case object Scala213Libraries extends Library(Scala213Versions)
 
 }
