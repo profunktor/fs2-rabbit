543afbd69941582dc03f0c9d73ce78efc547195e
Changlin Li <mail@changlinli.com>
2019-06-28 14:24:51 -0400

    Add comment explaining presence of Try
    It's otherwise weird to have it in what should be pure code.

--- core/src/main/scala/dev/profunktor/fs2rabbit/interpreter/AMQPClientEffect.scala
+++ core/src/main/scala/dev/profunktor/fs2rabbit/interpreter/AMQPClientEffect.scala
@@ -57,6 +57,11 @@ class AmqpClientEffect[F[_]: Effect] extends AMQPClient[F] {
           val routingKey  = RoutingKey(envelope.getRoutingKey)
           val exchange    = ExchangeName(envelope.getExchange)
           val redelivered = envelope.isRedeliver
+          // This is the main point of wrapping this whole thing in a Try; it
+          // is very unlikely any of the other parts will go wrong, unsafeFrom
+          // might though (if it does it's a bug that needs to be fixed!), but
+          // we don't want a bug in the usage of unsafeFrom to bring down our
+          // entire queue
           val props       = AmqpProperties.unsafeFrom(properties)
           AmqpEnvelope(DeliveryTag(tag), body, props, exchange, routingKey, redelivered)
         }.toEither
