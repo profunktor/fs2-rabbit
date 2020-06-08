17960cf0c43101e0ff8e0b07c2713e30d6210c11
Scala Steward <me@scala-steward.org>
2020-06-09 00:57:47 +0200

    Update fs2-core to 2.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Version {
     val catsEffect = "2.1.2"
-    val fs2        = "2.3.0"
+    val fs2        = "2.4.0"
     val circe      = "0.13.0"
     val amqpClient = "5.9.0"
     val logback    = "1.2.3"
