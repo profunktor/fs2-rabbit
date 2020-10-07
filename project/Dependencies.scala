c8a47c91cbaf024ceab04354e3604e7e89f7add2
Scala Steward <me@scala-steward.org>
2020-10-07 04:52:52 +0200

    Update zio-interop-cats to 2.2.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val logback    = "1.2.3"
     val monix      = "3.2.2"
     val zio        = "1.0.2"
-    val zioCats    = "2.2.0.0"
+    val zioCats    = "2.2.0.1"
     val scodec     = "1.0.0"
     val dropwizard = "4.1.12.1"
 
