125b8b9f7f6e460a19ff54b6881ec65659c973b9
Scala Steward <me@scala-steward.org>
2020-11-13 10:50:51 +0100

    Update metrics-core, metrics-jmx to 4.1.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val zio        = "1.0.3"
     val zioCats    = "2.2.0.1"
     val scodec     = "1.0.0"
-    val dropwizard = "4.1.14"
+    val dropwizard = "4.1.15"
 
     val kindProjector    = "0.11.0"
     val betterMonadicFor = "0.3.1"
