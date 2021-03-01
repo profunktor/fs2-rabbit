6e69af1cb8458d7610b44cf8b0dd8af7a7e4280b
jbwheatley <jackbwheatley@gmail.com>
2021-03-01 15:19:35 +0000

    Remove overloading 4.x (#497)
    * Move overloading of functions from Binding to RabbitClient
    
    Co-authored-by: Andrew Gustafson <gus4000@gmail.com>

--- core/src/main/scala/dev/profunktor/fs2rabbit/interpreter/RabbitClient.scala
+++ core/src/main/scala/dev/profunktor/fs2rabbit/interpreter/RabbitClient.scala
@@ -197,7 +197,7 @@ class RabbitClient[F[_]: Concurrent] private[fs2rabbit] (
       exchangeName: ExchangeName,
       routingKey: RoutingKey
   )(implicit channel: AMQPChannel): F[Unit] =
-    binding.bindQueue(channel, queueName, exchangeName, routingKey)
+    bindQueue(queueName, exchangeName, routingKey, QueueBindingArgs(Map.empty))
 
   def bindQueue(
       queueName: QueueName,
