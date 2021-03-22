59f56d6e26ca9ab9c38798a55e93a86d3f6a63f1
Scala Steward <me@scala-steward.org>
2021-03-22 13:48:36 +0100

    Update cats-kernel-laws, cats-laws to 2.4.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object Version {
-    val cats       = "2.3.1"
+    val cats       = "2.4.2"
     val catsEffect = "2.4.0"
     val fs2        = "2.5.2"
     val circe      = "0.13.0"
