fe88dcd0abae8585ee0d487cb6f9bf1c51a56558
Scala Steward <me@scala-steward.org>
2021-05-18 09:01:51 +0200

    Update zio to 1.0.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val amqpClient = "5.12.0"
     val logback    = "1.2.3"
     val monix      = "3.3.0"
-    val zio        = "1.0.7"
+    val zio        = "1.0.8"
     val zioCats    = "3.0.2.0"
     val scodec     = "1.1.0"
     val dropwizard = "4.2.0"
