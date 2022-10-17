e38a42c35ed8a7c88dd327442232333303409962
Scala Steward <scala_steward@virtuslab.com>
2022-10-17 23:51:52 +0000

    Update scodec-cats to 1.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val monix            = "3.3.0"
     val zio              = "1.0.17"
     val zioCats          = "3.2.9.1"
-    val scodec           = "1.1.0"
+    val scodec           = "1.2.0"
     val dropwizard       = "4.2.12"
     val collectionCompat = "2.7.0"
 
