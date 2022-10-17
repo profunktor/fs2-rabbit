8d06ba7bffa251460a258220b71535a728ce4c0e
Scala Steward <scala_steward@virtuslab.com>
2022-10-17 23:50:19 +0000

    Revert commit(s) 15a4e8a

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val catsEffect       = "3.3.12"
     val fs2              = "3.2.7"
     val circe            = "0.14.2"
-    val amqpClient       = "5.16.0"
+    val amqpClient       = "5.14.2"
     val logback          = "1.2.11"
     val monix            = "3.3.0"
     val zio              = "1.0.14"
