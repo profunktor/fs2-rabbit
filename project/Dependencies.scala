3d99bb074974042cc13613f63a7dbf2e196cc75f
Scala Steward <scala_steward@virtuslab.com>
2022-10-17 23:51:48 +0000

    Revert commit(s) e84b24d

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val monix            = "3.3.0"
     val zio              = "1.0.14"
     val zioCats          = "3.2.9.1"
-    val scodec           = "1.2.0"
+    val scodec           = "1.1.0"
     val dropwizard       = "4.2.9"
     val collectionCompat = "2.7.0"
 
