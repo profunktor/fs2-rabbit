2317426968980accd6117a8f31b929c39af8ff47
Scala Steward <scala_steward@virtuslab.com>
2022-11-20 18:51:20 +0000

    Update logback-classic to 1.4.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val fs2              = "3.3.0"
     val circe            = "0.14.3"
     val amqpClient       = "5.16.0"
-    val logback          = "1.4.4"
+    val logback          = "1.4.5"
     val monix            = "3.3.0"
     val zio              = "1.0.17"
     val zioCats          = "3.2.9.1"
