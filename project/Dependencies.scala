d5684722fea6dd5f85733b343fb3e3347ab167ce
Scala Steward <scala_steward@virtuslab.com>
2022-11-22 21:04:14 +0000

    Update fs2-core to 3.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Version {
     val cats             = "2.9.0"
     val catsEffect       = "3.4.1"
-    val fs2              = "3.3.0"
+    val fs2              = "3.4.0"
     val circe            = "0.14.3"
     val amqpClient       = "5.16.0"
     val logback          = "1.4.5"
