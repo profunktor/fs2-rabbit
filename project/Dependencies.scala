7575ba125fa5ab5abf3db3bbd67ef36314ae3593
Scala Steward <scala_steward@virtuslab.com>
2023-06-20 17:56:40 +0000

    Update amqp-client to 5.18.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val catsEffect       = "3.5.0"
     val fs2              = "3.7.0"
     val circe            = "0.14.5"
-    val amqpClient       = "5.17.0"
+    val amqpClient       = "5.18.0"
     val logback          = "1.4.8"
     val monix            = "3.3.0"
     val zio              = "1.0.18"
