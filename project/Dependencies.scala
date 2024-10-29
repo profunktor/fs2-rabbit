e847c811ddadb5d9caff87b795709f195bcff635
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-10-29 09:00:32 +0100

    Update cats-effect to 3.5.5 (#1006)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Version {
     val cats             = "2.10.0"
-    val catsEffect       = "3.5.4"
+    val catsEffect       = "3.5.5"
     val fs2              = "3.11.0"
     val circe            = "0.14.10"
     val amqpClient       = "5.22.0"
