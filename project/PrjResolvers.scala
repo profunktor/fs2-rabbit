77b1b25c56b43395334085f1428859a08558c817
Matej Cerny <cerny.matej@gmail.com>
2026-05-20 11:33:52 +0200

    Update sbt-ci-release to 1.11.2

--- project/PrjResolvers.scala
+++ project/PrjResolvers.scala
@@ -1,14 +1,9 @@
-import sbt.{Resolver, _}
+import sbt._
 import sbt.librarymanagement.MavenRepository
 
 object PrjResolvers {
   lazy val all: Seq[MavenRepository] = Seq(
-    Resolver.sonatypeOssRepos("public"),
-    Resolver.sonatypeOssRepos("snapshots"),
-    Resolver.sonatypeOssRepos("releases"),
-    Seq(
-      "Maven repo1" at "https://repo1.maven.org/",
-      "Maven repo2" at "https://mvnrepository.com/artifact"
-    )
-  ).flatten
+    "Maven repo1" at "https://repo1.maven.org/",
+    "Maven repo2" at "https://mvnrepository.com/artifact"
+  )
 }
